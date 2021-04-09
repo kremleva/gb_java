7a4a51460ed368da7622657072ed1296385a94e4
kremleva <77212001+kremleva@users.noreply.github.com>
2021-04-09 14:20:00 +0300

    Add files via upload

--- New_homework.java
+++ New_homework.java
@@ -1,52 +1,136 @@
 package geekbrains.lesson.kremleva;
-import java.util.Arrays;
-public class Main {
+import java.util.Random;
+import java.util.Scanner;
+
+public class New_homework {
+
+    public static char[][] field;
+    public static int fieldSizeX;
+    public static int fieldSizeY;
+
+    public static char human = 'X';
+    public static char ai = 'O';
+    public static char empty = '_';
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static Random random = new Random();
+
+
+    public static void createField() {
+        fieldSizeX = 5;
+        fieldSizeY = 5;
+        field = new char[fieldSizeY][fieldSizeX];
+
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = empty;
+            }
+        }
+    }
+
+    public static void viewField() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                System.out.print(field[y][x] + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("-------");
+    }
+    public static void turnHuman() {
+        int cellX;
+        int cellY;
+
+        do {
+            System.out.println("Enter coordinates cell: ");
+            cellX = scanner.nextInt() - 1;
+            cellY = scanner.nextInt() - 1;
+        } while (!validCell(cellY, cellX) || !emptyCell(cellY, cellX));
+        field[cellY][cellX] = human;
+    }
+
+    public static void turnAI() {
+        int cellX;
+        int cellY;
+
+        do {
+            cellX = random.nextInt(fieldSizeX);
+            cellY = random.nextInt(fieldSizeY);
+        } while (!emptyCell(cellY, cellX));
+        field[cellY][cellX] = ai;
+    }
+
+    public static boolean winGame(char player) {
+        if (field[0][0] == player && field[1][1] == player && field[2][2] == player && field[3][3] == player) return true;
+        if (field[0][0] == player && field[0][1] == player && field[0][2] == player && field[0][3] == player) return true;
+        if (field[1][0] == player && field[1][1] == player && field[1][2] == player && field[1][3] == player) return true;
+        if (field[2][0] == player && field[2][1] == player && field[2][2] == player && field[2][3] == player) return true;
+        if (field[3][0] == player && field[3][1] == player && field[3][2] == player && field[3][3] == player) return true;
+        if (field[4][0] == player && field[4][1] == player && field[4][2] == player && field[4][3] == player) return true;
+        if (field[0][0] == player && field[1][0] == player && field[2][0] == player && field[3][0] == player) return true;
+        if (field[1][0] == player && field[2][0] == player && field[3][0] == player && field[4][0] == player) return true;
+        if (field[1][1] == player && field[2][1] == player && field[3][1] == player && field[4][1] == player) return true;
+        if (field[1][0] == player && field[2][0] == player && field[3][0] == player && field[4][0] == player) return true;
+        if (field[1][1] == player && field[1][2] == player && field[1][3] == player && field[1][4] == player) return true;
+        if (field[2][1] == player && field[2][2] == player && field[2][3] == player && field[2][4] == player) return true;
+        if (field[3][1] == player && field[3][2] == player && field[3][3] == player && field[3][4] == player) return true;
+        if (field[4][1] == player && field[4][2] == player && field[4][3] == player && field[4][4] == player) return true;
+        if (field[3][1] == player && field[2][2] == player && field[1][3] == player && field[0][4] == player) return true;
+        if (field[4][1] == player && field[3][2] == player && field[2][3] == player && field[1][4] == player) return true;
+        if (field[3][0] == player && field[2][1] == player && field[1][2] == player && field[0][3] == player) return true;
+        if (field[0][1] == player && field[1][2] == player && field[2][3] == player && field[3][4] == player) return true;
+        return false;
+    }
+    public static boolean draw() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (field[y][x] == empty) return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean validCell(int cellY, int cellX) {
+        return cellX >= 0 && cellX < fieldSizeX && cellY >= 0 && cellY < fieldSizeY;
+    }
+
+    public static boolean emptyCell(int cellY, int cellX) {
+        return field[cellY][cellX] == empty;
+    }
 
     public static void main(String[] args) {
 
-      int[] myArray = {1, 0, 0, 1, 0, 0, 1, 0, 1, 0};
-      public static int (int[] myArray) {
-          for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = (myArray[i] == 0) ? 1 : 0; }
-         System.out.println(myArray);
-     }
-     }
+        createField();
+        viewField();
 
-     public static void (int[] array) {
-         int [] array2 = new int [8];
-      for (int i = 0; i < array2.length; i = i + 3) {
-        array2[i] = "Value" + i;
+        while(true) {
+
+            turnHuman();
+            viewField();
+
+            if (winGame(human)) {
+                System.out.println("Human win!");
+                break;
+            }
+
+            if (draw()) {
+                System.out.println("Draw!");
+                break;
+            }
+
+            turnAI();
+            viewField();
+
+            if (winGame(ai)) {
+                System.out.println("AI win!");
+                break;
+            }
+
+            if (draw()) {
+                System.out.println("Draw!");
+                break;
+            }
+        }
     }
-    int i;
-    for (i = 0; i < array2.length; i = i + 3) {
-      System.out.println(array2[i] + " | "); }
 }
-    int[] array = [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ];
-    public static void (int[] array) {
-
-      for (int i = 0; i < array.length; i++) {
-        if (array[i] < 6) {
-          array[i] *= 2;
-}
-        }
-        System.out.println(array[i]);
-    }
-
-    public static int arrayMin(int[] array){
-        int min = array[0];
-        for(int i = 0; i < array.length; i++){
-            if (min > array[i])
-                min = array[i];
-        }
-        return min;
-    }
-    public static int arrayMax(int[] array){
-        int max = array[0];
-        for(int i = 0; i < array.length; i++){
-            if (max < array[i])
-                max = array[i];
-        }
-        return max;
-    }
-    }
-
